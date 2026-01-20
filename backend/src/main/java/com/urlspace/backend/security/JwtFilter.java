1143ebd0e488b3ffab203d6c81878774f5be0913
Manideep Thippani <thippani.manideep@eil.co.in>
2026-01-20 10:26:51 +0530

    feat: secure URL APIs using JWT authentication

---
+++ backend/src/main/java/com/urlspace/backend/security/JwtFilter.java
@@ -0,0 +1,38 @@
+package com.urlspace.backend.security;
+
+import java.io.IOException;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        // JWT validation logic to be implemented
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Missing or Inavalid JWT Token");
+            return;
+        }
+        String token = authHeader.substring(7);
+
+        try {
+            String email = JwtUtil.validateToken(token);
+            request.setAttribute("email", email);
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Invalid JWT");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
--- backend/src/main/java/com/urlspace/backend/security/JwtUtil.java
+++ backend/src/main/java/com/urlspace/backend/security/JwtUtil.java
@@ -9,13 +9,21 @@ public class JwtUtil {
 
     private static final String SECRET_KEY = "urlspace_secret_key";
 
-    public static String generateToken(String email){
+    public static String generateToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 2)) // 2 hours
-                .signWith(SignatureAlgorithm.HS256,SECRET_KEY)
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
     }
-    
+
+    public static String validateToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
 }
